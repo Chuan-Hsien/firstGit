c1127c51c20a53d6f663116267a9fb64cbeb3df0
Chiu Chuan-Hsien <chiucs@chipright.com.tw>
2023-05-01 21:46:29 +0800

    qt create a button to load image only.

--- qt_form.py
+++ qt_form.py
@@ -1,1 +1,33 @@
-import PySide6
+from PySide6 import QtCore
+from PySide6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout
+from PySide6.QtWidgets import QPushButton
+import numpy as np
+import cv2 as cv
+
+class AdjustDialog(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.msg = "Hello world!"
+        self.lbl = QLabel(self.msg)
+
+        self.cmd = QPushButton("Click")
+
+        self.layout = QVBoxLayout(self)
+        self.layout.addWidget(self.lbl)
+        self.layout.addWidget(self.cmd)
+
+        self.cmd.clicked.connect(self.load_img)
+    
+    @QtCore.Slot()
+    def load_img(self):
+        self.read_frame = cv.imread("jam4.jpg")
+        cv.imshow("OpenCV", self.read_frame)
+
+if __name__ == "__main__":
+    app = QApplication([])
+    form = AdjustDialog()
+    form.resize(300,300)
+    form.show()
+
+    app.exec_()
